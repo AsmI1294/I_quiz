088fa92c49e4079c5a28db60eb0e09c03d5c43ce
ASMIttehad <ittehad.akash@gmail.com>
2022-10-26 21:24:59 +0200

    card finished

--- src/components/EachCard/EachCard.js
+++ src/components/EachCard/EachCard.js
@@ -2,17 +2,19 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
-const EachCard = () => {
+const EachCard = ({ data }) => {
+  console.log(data);
   return (
-    <Card style={{ width: "20rem" }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+    <Card style={{ width: "18rem" }} bg={"dark"} text={"white"}>
+      <Card.Img
+        variant="top"
+        src={data.logo}
+        className="border border-secondary rounded shadow-lg"
+      />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="dark" className="w-100">
+        <Card.Title>{data.name}</Card.Title>
+        <Card.Text>Number of questions: {data.total}</Card.Text>
+        <Button variant="info" className="w-100">
           Go somewhere
         </Button>
       </Card.Body>
--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -5,15 +5,22 @@ import { Col, Container, Row } from "react-bootstrap";
 import EachCard from "../EachCard/EachCard";
 const Topics = () => {
   let { data } = useLoaderData();
-  console.log(data);
+
   return (
     <div>
       <Header></Header>
       <Container className="my-5">
         <Row>
-          {data.map(() => (
-            <Col sm={6} className=" d-flex justify-content-center my-4">
-              <EachCard></EachCard>
+          {data.map((eachData) => (
+            <Col
+              lg={3}
+              md={6}
+              sm={12}
+              key={eachData.id}
+              className=" d-flex justify-content-center my-4"
+            >
+              <EachCard data={eachData}></EachCard>
+              {console.log(eachData.id)}
             </Col>
           ))}
         </Row>
