b87de122127f1eb553e7a861f02f5488e929bda2
ASMIttehad <ittehad.akash@gmail.com>
2022-10-28 13:31:43 +0200

    quiz finished

--- src/components/Quizes/Quizes.js
+++ src/components/Quizes/Quizes.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
 
 import { useLoaderData } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
 import Quiz from "../Quiz/Quiz";
 
 const Quizes = () => {
@@ -14,15 +15,44 @@ const Quizes = () => {
     setState(newState);
     console.log(newState);
   }
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  const submit = () => {
+    if (state.includes(undefined)) {
+      window.scrollTo(0, 0);
+
+      document.getElementById(
+        "score"
+      ).innerHTML = `Score: 0/${questions.length}`;
+    } else {
+      let Total = state.reduce(
+        (previousValue, currentValue) => previousValue + currentValue,
+        0
+      );
+      document.getElementById(
+        "score"
+      ).innerHTML = `Score: ${Total}/${questions.length}`;
+      window.scrollTo(0, 0);
+    }
+  };
   return (
     <div>
+      <div
+        id="score"
+        className="text-center display-3 bg-info fw-semibold text-secondary"
+      ></div>
       <div>
         {questions.map((ques, id) => (
           <Quiz key={ques.id} id={id} isCorrect={isCorrect} ques={ques}></Quiz>
         ))}
       </div>
       <div className="d-flex justify-content-center m-5 ">
-        <Button variant="info" className="w-50 text-light fs-4">
+        <Button
+          variant="info"
+          className="w-50 text-light fs-4"
+          onClick={submit}
+        >
           Submit
         </Button>
       </div>
