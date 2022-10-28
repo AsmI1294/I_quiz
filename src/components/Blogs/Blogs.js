b541bed02ff454a162e1f0cc703908418dbbab7c
ASMIttehad <ittehad.akash@gmail.com>
2022-10-28 16:40:33 +0200

    Main part finished

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,8 +1,55 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 import Accordion from "react-bootstrap/Accordion";
 
 const Blogs = () => {
-  return <div>blogs</div>;
+  return (
+    <Container>
+      <p className="text-center display-3 bg-info fw-semibold text-secondary">
+        Answer to few Questions
+      </p>
+      <Accordion>
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>
+            <p className=" text-secondary fw-bold fs-2">
+              What is the purpose of react router?
+            </p>
+          </Accordion.Header>
+          <Accordion.Body>
+            React Router is a JavaScript framework that lets us handle client
+            and server-side routing in React applications. It enables the
+            creation of single-page web or mobile apps that allow navigating
+            without refreshing the page.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>
+            <p className=" text-secondary fw-bold fs-2">
+              How does context API work?
+            </p>
+          </Accordion.Header>
+          <Accordion.Body>
+            The React Context API is a way for a React app to effectively
+            produce global variables that can be passed around. This is the
+            alternative to "prop drilling" or moving props from grandparent to
+            child to parent, and so on. We can also pass states in its range.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="2">
+          <Accordion.Header>
+            <p className=" text-secondary fw-bold fs-2">
+              What is useref hook in react?
+            </p>
+          </Accordion.Header>
+          <Accordion.Body>
+            The useRef Hook allows you to persist values between renders. It can
+            be used to store a mutable value that does not cause a re-render
+            when updated. It can be used to access a DOM element directly.
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+    </Container>
+  );
 };
 
 export default Blogs;
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -16,25 +16,30 @@ const Statistics = () => {
   console.log(data);
 
   return (
-    <div className="d-flex justify-content-center">
-      <ResponsiveContainer width="70%" height={500}>
-        <BarChart
-          data={data}
-          margin={{
-            top: 20,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis></YAxis>
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="total" stackId="a" fill="#8884d8" />
-        </BarChart>
-      </ResponsiveContainer>
+    <div>
+      <p className="text-center display-3 bg-info fw-semibold text-secondary py-3">
+        Number of quizes on all the topics
+      </p>
+      <div className="d-flex justify-content-center">
+        <ResponsiveContainer width="70%" height={500}>
+          <BarChart
+            data={data}
+            margin={{
+              top: 20,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis></YAxis>
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="total" stackId="a" fill="#8884d8" />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 };
